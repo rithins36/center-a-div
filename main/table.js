ca130c9a132724ecbc35666f298581a9deff04f5
Rithin Samuel <rithinsamuel12@gmail.com>
2023-02-02 11:43:45 +0530

    Updates

---
+++ main/table.js
@@ -0,0 +1,16 @@
+function loadScoresGSheet() {
+
+    let sheetUrl = "https://docs.google.com/spreadsheets/d/1IBrHZ5zt5EP9TILd-YTCbHVipz6A8dnXheZsF_VcmVc/edit#gid=0";
+  
+    $('#scores').sheetrock(
+      {
+        url: sheetUrl,
+        query: `SELECT A, B WHERE B >= 20 order by B desc`,
+        labels: ["NAME", "POINTS"],
+        callback: () => {},
+      }
+    );
+  };
+  
+  
+  $(document).ready(loadScoresGSheet);
